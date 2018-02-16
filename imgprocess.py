1fedcf613a9656369fcf826108c067fce27e70b5
AruranV <35665765+AruranV@users.noreply.github.com>
2018-02-15 21:19:01 -0500

    Add files via upload

---
+++ imgprocess.py
@@ -0,0 +1,53 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('ISIC_0001100.jpg')
+
+#edges = cv2.Canny(img,100,200)
+#
+#plt.subplot(121),plt.imshow(img,cmap = 'gray')
+#plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+#plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+#plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+#
+#plt.show()
+
+#img = np.float32(img) / 255.0
+# 
+## Calculate gradient 
+#gx = cv2.Sobel(img, cv2.CV_32F, 1, 0, ksize=1)
+#gy = cv2.Sobel(img, cv2.CV_32F, 0, 1, ksize=1)
+#
+## Python Calculate gradient magnitude and direction ( in degrees ) 
+#mag, angle = cv2.cartToPolar(gx, gy, angleInDegrees=True)
+
+#laplacian = cv2.Laplacian(img,cv2.CV_64F)
+#sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)
+#sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)
+
+#plt.subplot(2,2,1),plt.imshow(img,cmap = 'gray')
+#plt.title('Original'), plt.xticks([]), plt.yticks([])
+#plt.subplot(2,2,2),plt.imshow(laplacian,cmap = 'gray')
+#plt.title('Laplacian'), plt.xticks([]), plt.yticks([])
+#plt.subplot(2,2,3),plt.imshow(sobelx,cmap = 'gray')
+#plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
+#plt.subplot(2,2,4),plt.imshow(sobely,cmap = 'gray')
+#plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
+
+plt.show()
+ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+ret,thresh2 = cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV)
+ret,thresh3 = cv2.threshold(img,127,255,cv2.THRESH_TRUNC)
+ret,thresh4 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO)
+ret,thresh5 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO_INV)
+
+titles = ['Original Image','BINARY','BINARY_INV','TRUNC','TOZERO','TOZERO_INV']
+images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
+
+for i in range(6):
+    plt.subplot(2,3,i+1),plt.imshow(images[i],'gray')
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
+
+plt.show()
